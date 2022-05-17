2d7e307bfc40dca2e49f0d23d1e4623931f097f9
jszuluaga <johanas_zuluaga@hotmail.com>
2022-05-16 21:41:13 -0500

    se modifica

--- asientos.js
+++ asientos.js
@@ -2,7 +2,7 @@ function asientosDisponibles (asiento, ocupar){
   if ( asiento.indexOf(ocupar) >= 0){
 return "Felicitaciones, el asiento número" + " " + ocupar + " " + "está disponible"
 } else {
-return "Lo sentimos, el asiento número" + " " + ocupar + " " + "está ocupado, pero aún quedan" + " " + asiento.length + " " + "asientos disponibles"
+return "Lo sentimos , el asiento número" + " " + ocupar + " " + "está ocupado, pero aún quedan" + " " + asiento.length + " " + "asientos disponibles"
 }
 }
 console.log(asientosDisponibles([25,30,5,2,4],100));
